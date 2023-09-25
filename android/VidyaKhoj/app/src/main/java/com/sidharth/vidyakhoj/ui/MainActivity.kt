e019e235692044caa04de4a3963196008bff38f9
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-09-25 15:37:49 +0530

    bugfixes: VidyaKhoj app

--- android/VidyaKhoj/app/src/main/java/com/sidharth/vidyakhoj/ui/MainActivity.kt
+++ android/VidyaKhoj/app/src/main/java/com/sidharth/vidyakhoj/ui/MainActivity.kt
@@ -3,9 +3,12 @@ package com.sidharth.vidyakhoj.ui
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.sidharth.vidyakhoj.databinding.ActivityMainBinding
 import com.sidharth.vidyakhoj.service.RefreshDataService
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -16,9 +19,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(mainBinding.root)
-        Intent(this, RefreshDataService::class.java).apply {
-            action = RefreshDataService.Action.START.toString()
-            startService(this)
+        lifecycleScope.launch {
+            Intent(this@MainActivity, RefreshDataService::class.java).apply {
+                action = RefreshDataService.Action.START.toString()
+                delay(10000)
+                startService(this)
+            }
         }
     }
 }
