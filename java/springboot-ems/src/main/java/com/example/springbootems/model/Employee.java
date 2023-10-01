a6d54847ced3c41cf1a54ca69464c6b812fa1bc3
TharunBalaji2004 <tharunbalaji31@gmail.com>
2023-10-01 11:18:19 +0530

    Added Model Class

--- java/springboot-ems/src/main/java/com/example/springbootems/model/Employee.java
+++ java/springboot-ems/src/main/java/com/example/springbootems/model/Employee.java
@@ -1,5 +1,32 @@
 package main.java.com.example.springbootems.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "Employee")
 public class Employee {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long empId;
+    public String empName;
+    public String empEmail;
+    public String empPhone;
+    public String empDept;
+    public Double empSalary;
+
+    public Employee(String empName, String empEmail, String empPhone, String empDept, Double empSalary) {
+        this.empName = empName;
+        this.empEmail = empEmail;
+        this.empPhone = empPhone;
+        this.empDept = empDept;
+        this.empSalary = empSalary;
+    }
+
+    public Employee() {
+
+    }
 }
