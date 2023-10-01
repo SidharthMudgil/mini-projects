6fd5af341a570adc4360651d401c314b8fe22a50
TharunBalaji2004 <tharunbalaji31@gmail.com>
2023-10-01 11:04:24 +0530

    Added Repository class with JPA

--- java/springboot-rest-crud/src/main/java/com/example/springbootrestcrud/repository/StudentRepository.java
+++ java/springboot-rest-crud/src/main/java/com/example/springbootrestcrud/repository/StudentRepository.java
@@ -1,5 +1,10 @@
 package com.example.springbootrestcrud.repository;
+import com.example.springbootrestcrud.model.StudentModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class StudentRepository {
-    
+@Repository
+public interface StudentRepository extends JpaRepository<StudentModel, Integer> {
+
 }
+
