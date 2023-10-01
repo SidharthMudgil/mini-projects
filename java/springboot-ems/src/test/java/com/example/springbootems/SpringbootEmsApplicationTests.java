f0cc803e6a4b872543db901b21967f97118ec7f9
TharunBalaji2004 <tharunbalaji31@gmail.com>
2023-10-01 11:15:21 +0530

    Initial Commit

---
+++ java/springboot-ems/src/test/java/com/example/springbootems/SpringbootEmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
