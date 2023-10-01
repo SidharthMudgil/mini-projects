6e7a7c4d2296e7b6cb5edc6ce4cf85e94a05bcc2
TharunBalaji2004 <tharunbalaji31@gmail.com>
2023-10-01 11:18:53 +0530

    Implemented Controller class with REST endpoints

--- java/springboot-ems/src/main/java/com/example/springbootems/controller/EMSController.java
+++ java/springboot-ems/src/main/java/com/example/springbootems/controller/EMSController.java
@@ -1,5 +1,39 @@
 package main.java.com.example.springbootems.controller;
 
+import com.tharunbalaji.springbootems.model.Employee;
+import com.tharunbalaji.springbootems.repository.EMSRepository;
+import com.tharunbalaji.springbootems.service.EMSService;
+import com.tharunbalaji.springbootems.service.EMSServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class EMSController {
-    
+
+    @Autowired
+    private EMSServiceImpl emsService;
+
+    @GetMapping("/ems")
+    public String listEmployees(Model model) {
+        model.addAttribute("employees", emsService.getAllEmployees());
+        return "employees";
+    }
+
+    @GetMapping("/ems/add")
+    public String addEmployee(Model model) {
+        // Employee object to hold data
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "add_employee";
+    }
+
+    @PostMapping("/ems/add")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        emsService.saveStudent(employee);
+        return "redirect:/ems";
+    }
 }
