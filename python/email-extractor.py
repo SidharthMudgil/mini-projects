cc9d4725c3d43fb53786e04a8b43fce2136c6c81
animesh chaudhri <111721851+animeshchaudhri@users.noreply.github.com>
2023-10-01 12:54:37 +0530

    Implement Input Validation and Enhanced Regular Expression for Email Extraction
    I have introduced input validation to ensure the provided text is a valid non-empty string before attempting email extraction. Additionally, I have updated the regular expression pattern used for email extraction to a more comprehensive one, improving the accuracy of email address identification. These changes enhance the code's reliability and accuracy in extracting email addresses from user-provided text, making it more robust for various input scenarios.
    Fixes #13

--- python/email-extractor.py
+++ python/email-extractor.py
@@ -1,19 +1,19 @@
 import re
 
+def extract_emails(text):
+    # Improved regular expression pattern for email extraction
+    email_pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b"
 
-# function to extract email from the text
-def extract(text):
-    # The pattern of email stored in a regex
-    mail = r"[\w+_%]+@[\S]+\.+[\w+_%]+"
+    # Finding all matched email patterns
+    emails = re.findall(email_pattern, text)
 
-    # finding all matched patterns
-    emails = re.findall(mail, text)
-
-    # printing all matched patterns
-    for email in emails:
-        print(email)
-
+    # Checking if any emails were found
+    if emails:
+        for email in emails:
+            print(email)
+    else:
+        print("No valid email addresses found in the provided text.")
 
 if __name__ == "__main__":
-    string = input("->")
-    extract(string)
+    string = input("Enter a text to extract email addresses from: ")
+    extract_emails(string)
