f75316cb2eb5f2e3f6c7ac49f592b94d957951b0
TharunBalaji2004 <tharunbalaji31@gmail.com>
2023-10-01 11:02:58 +0530

    Added Model class

--- java/springboot-rest-crud/src/main/java/com/example/springbootrestcrud/model/StudentModel.java
+++ java/springboot-rest-crud/src/main/java/com/example/springbootrestcrud/model/StudentModel.java
@@ -1,5 +1,18 @@
 package com.example.springbootrestcrud.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data // By adding this annotator getters and setters need not be created manually
+@Entity // represents the class as Model Class and maps the class to database
+@Table(name = "Students")
 public class StudentModel {
-    
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    public String address;
+    private int age;
 }
