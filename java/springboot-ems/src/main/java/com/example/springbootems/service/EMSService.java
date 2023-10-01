4c1ad705137b57b62a63c62fb60a044d76fcd609
TharunBalaji2004 <tharunbalaji31@gmail.com>
2023-10-01 11:19:46 +0530

    Added Service class

--- java/springboot-ems/src/main/java/com/example/springbootems/service/EMSService.java
+++ java/springboot-ems/src/main/java/com/example/springbootems/service/EMSService.java
@@ -1,5 +1,17 @@
 package main.java.com.example.springbootems.service;
 
+import com.example.springbootems.model.Employee;
+import com.example.springbootems.repository.EMSRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface EMSService {
-    
+
+    List<Employee> getAllEmployees();
+
+    void saveStudent(Employee employee);
 }
+
