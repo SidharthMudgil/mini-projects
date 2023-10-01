a3cd15ebb0396fcb97e0ded59789230c8d38a142
TharunBalaji2004 <tharunbalaji31@gmail.com>
2023-10-01 11:21:15 +0530

    Added SQL and Thymeleaf configuration

--- java/springboot-ems/src/main/java/com/example/springbootems/controller/EMSController.java
+++ java/springboot-ems/src/main/java/com/example/springbootems/controller/EMSController.java
@@ -1,9 +1,9 @@
 package main.java.com.example.springbootems.controller;
 
-import com.tharunbalaji.springbootems.model.Employee;
-import com.tharunbalaji.springbootems.repository.EMSRepository;
-import com.tharunbalaji.springbootems.service.EMSService;
-import com.tharunbalaji.springbootems.service.EMSServiceImpl;
+import com.example.springbootems.model.Employee;
+import com.example.springbootems.repository.EMSRepository;
+import com.example.springbootems.service.EMSService;
+import com.example.springbootems.service.EMSServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
