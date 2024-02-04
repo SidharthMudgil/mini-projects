d5208d14ba68d4916f844e3e2878a1fd8ca8b76c
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2024-02-04 17:42:04 +0530

    balloon is working now

--- flutter/lg_buttons/lib/service/kml_helper.dart
+++ flutter/lg_buttons/lib/service/kml_helper.dart
@@ -10,7 +10,7 @@ _orbitTag() {
               <gx:duration>1.2</gx:duration>
               <gx:flyToMode>smooth</gx:flyToMode>
               <LookAt>
-                 <LookAt><longitude>${76.6084}</longitude><latitude>${28.8958}</latitude><altitude>200</altitude><range>${3000}</range><tilt>70</tilt><heading>$heading</heading><gx:altitudeMode>relativeToGround</gx:altitudeMode></LookAt>
+                 <LookAt><longitude>${76.6084}</longitude><latitude>${28.8958}</latitude><range>${2000}</range><tilt>70</tilt><heading>$heading</heading><gx:altitudeMode>relativeToGround</gx:altitudeMode></LookAt>
               </LookAt>
             </gx:FlyTo>
           ''';
@@ -41,21 +41,28 @@ String balloon() {
 <?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
 <Document>
- <name>balloon.kml</name>
- <Style id="purple_paddle">
+ <name>Task2</name>
+ <Style id="weather_style">
    <BalloonStyle>
-     <text>\$[description]</text>
-     <bgColor>fffcfcfc</bgColor>
+     <textColor>ffffffff</textColor>
+     <text>
+        <h1>Sidharth Mudgil</h1>
+        <h2>Rohtak, Haryana</h2>
+     </text>
+     <bgColor>ff15151a</bgColor>
    </BalloonStyle>
  </Style>
- <Placemark>
-   <name>Home town</name>
-   <Snippet maxLines="0"></Snippet>
-   <description><![CDATA[<
-   <h1>Sidharth Mudgil</h1>
-   <h2>Rohtak, Haryana, India</h2>
-]]></description>
-   <styleUrl>#purple_paddle</styleUrl>
+ <Placemark id="ww">
+   <description>
+   </description>
+   <LookAt>
+     <longitude>76.6084</longitude>
+     <latitude>28.8958</latitude>
+     <heading>0</heading>
+     <tilt>70</tilt>
+     <range>4000000</range>
+   </LookAt>
+   <styleUrl>#weather_style</styleUrl>
    <gx:balloonVisibility>1</gx:balloonVisibility>
    <Point>
      <coordinates>76.6084,28.8958,0</coordinates>
--- flutter/lg_buttons/lib/service/lg_service.dart
+++ flutter/lg_buttons/lib/service/lg_service.dart
@@ -10,16 +10,8 @@ class LGService {
   int _port;
   int _slaves;
 
-  String get _dataSlave {
-    if (_slaves == 1) {
-      return "slave_1";
-    } else {
-      return "slave_${(_slaves / 2) + 1}";
-    }
-  }
-
   String get _lookAt {
-    return "<LookAt><longitude>${76.6084}</longitude><latitude>${28.8958}</latitude><range>${3000}</range><tilt>${0}</tilt><heading>${0}</heading><gx:altitudeMode>relativeToGround</gx:altitudeMode></LookAt>";
+    return "<LookAt><longitude>${76.6084}</longitude><latitude>${28.8958}</latitude><range>${3000}</range><tilt>${0}</tilt><gx:fovy>60</gx:fovy><heading>${0}</heading><gx:altitudeMode>relativeToGround</gx:altitudeMode></LookAt>";
   }
 
   factory LGService({
@@ -102,7 +94,6 @@ class LGService {
 
   Future<bool> moveOrbitMyCity() async {
     try {
-      moveToHomeCity();
       await _execute("echo '${orbit()}' > /var/www/html/homecity.kml");
       await  _execute('echo "http://lg1:81/homecity.kml" >> /var/www/html/kmls.txt');
       return await _execute("echo 'playtour=Orbit' > /tmp/query.txt");
@@ -113,7 +104,9 @@ class LGService {
 
   Future<bool> showBubble() async {
     try {
-      return await _execute("echo '${balloon()}' > /var/www/html/kml/$_dataSlave.kml");
+      await _execute("echo '${balloon()}' > /var/www/html/bubble.kml");
+      await  _execute('echo "http://lg1:81/bubble.kml" >> /var/www/html/kmls.txt');
+      return true;
     } catch (e) {
       return false;
     }
